048655f6c4cb66acc0c7645ea89140ece9d66ccb
penpensun <penpen926@gmail.com>
2018-11-04 22:28:15 +0100

    security front end

---
+++ frontend_playground/vue_springboot_nginx/back_end/websecurityexample/src/main/java/org/peng/web/TreeViewController.java
@@ -0,0 +1,7 @@
+package org.peng.web;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@RequestMapping("/rest/")
+public class TreeViewController {
+
+}
---
+++ frontend_playground/vue_springboot_nginx/vue_app/src/components/TreeView.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'TreeView',
+  
+}
+</script>
+<style scoped>
+
+</style>
