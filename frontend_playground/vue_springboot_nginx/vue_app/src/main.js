62012e87dd4502fa3556c2e70c0159fa1b8e6491
Peng Sun <peng.sun2@bayer.com>
2018-11-11 21:45:41 +0100

    vue frontend shiro

--- frontend_playground/vue_springboot_nginx/vue_app/src/main.js
+++ frontend_playground/vue_springboot_nginx/vue_app/src/main.js
@@ -4,16 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import installAxios from './installAxios'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+installAxios()
 
 /* eslint-disable no-new */
 console.log(router.routes)
 new Vue({
   el: '#app',
   router,
-  // components: { App },
-  // template: '<App/>'\
-  render: h => h(App)
+  components: { App },
+  template: '<App/>'
+  // render: h => h(App)
 })
