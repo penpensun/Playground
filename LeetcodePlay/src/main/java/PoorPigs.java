d1e34b206c0e9245bea32357b4e2cebf03c207a1
penpensun <penpen926@gmail.com>
2018-12-03 07:13:19 +0100

    leetcode project added

---
+++ LeetcodePlay/src/main/java/PoorPigs.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+class PoorPigs {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int states= minutesToTest/minutesToDie+1;
+        return (int)Math.ceil(Math.log(buckets)/Math.log(states));
+    }
+
+    public static void main(String args[]){
+        int result = new PoorPigs().poorPigs(8, 15,45);
+        System.out.println(result);
+    }
+}
